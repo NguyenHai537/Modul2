d3882d0bfef1468070078d93781c7b3d592a4432
Your Name <you@example.com>
2022-11-09 16:41:13 +0700

    modul2.11/09-2file

---
+++ StopWatch.java
@@ -0,0 +1,25 @@
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+
+    public StopWatch(){}
+
+
+    public long start(){
+        this.startTime =System.currentTimeMillis();
+        return this.startTime;
+    }
+
+    public long stop(){
+        this.endTime = System.currentTimeMillis();
+        return this.endTime;
+    }
+
+    public long getElapsedTime(){
+        return this.endTime - this.startTime;
+    }
+
+
+}
