41d3f55a133ad6a04373df121fc0f02bab31e1a0
Your Name <you@example.com>
2022-11-10 16:23:44 +0700

    modul2. 11/10. classStudent

---
+++ TestStudent.java
@@ -0,0 +1,8 @@
+public class TestStudent {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        st1.setName("Hoa");
+        st1.setClasses("C01");
+        System.out.println(st1.getName() + " "+ st1.getClasses());
+    }
+}
