4b0b2f104e98020cdd509f5b73fc7dadaee9aba4
Your Name <you@example.com>
2022-11-11 22:21:35 +0700

    modul2.11/11.ClasTriangle

---
+++ TestTriagnle.java
@@ -0,0 +1,9 @@
+public class TestTriagnle {
+    public static void main(String[] args) {
+        BaiTapTrienKhaiVaThietKeClassTriangle triangle = new BaiTapTrienKhaiVaThietKeClassTriangle();
+        System.out.println(triangle);
+
+        triangle = new BaiTapTrienKhaiVaThietKeClassTriangle(3,4,5);
+        System.out.println(triangle);
+    }
+}
