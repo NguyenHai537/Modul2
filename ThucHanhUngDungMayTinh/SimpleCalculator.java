129a57c53c0cd926a5114f4a4d9b3cfff354241f
Your Name <you@example.com>
2022-11-16 10:01:06 +0700

    modul2.16/11.ThucHanhUngDUngMayTinh

---
+++ ThucHanhUngDungMayTinh/SimpleCalculator.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+
+    public static int mul(int first , int second) {return first * second ;}
+
+    public static int division(int first , int second) {return first / second;}
+}
