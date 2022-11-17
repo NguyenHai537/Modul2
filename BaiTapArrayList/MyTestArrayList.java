3dbd427f961074559f689cd3069d640f8adfa9eb
Your Name <you@example.com>
2022-11-17 15:32:21 +0700

    modul2.17/11.BaitapArrayList

---
+++ BaiTapArrayList/MyTestArrayList.java
@@ -0,0 +1,15 @@
+package BaiTapArrayList;
+
+public class MyTestArrayList {
+    public static void main(String[] args) {
+        MyList mylist = new MyList<>();
+        mylist.add(3);
+        mylist.add(4);
+        mylist.add(6);
+        mylist.add(7);
+        mylist.remove(2);
+
+        System.out.println();
+
+    }
+}
