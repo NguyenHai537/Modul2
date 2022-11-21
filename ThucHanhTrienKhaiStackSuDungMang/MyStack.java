94ec574babf5a7489c194cdf802ba072cc118024
Your Name <you@example.com>
2022-11-21 20:12:20 +0700

    modul2

---
+++ ThucHanhTrienKhaiStackSuDungMang/MyStack.java
@@ -0,0 +1,33 @@
+package ThucHanhTrienKhaiStackSuDungMang;
+
+public class MyStack {
+    private int arr[];
+    private int size;
+    private int index = 0;
+
+    public MyStack(int size) {
+        this.size = size;
+        arr = new int[size];
+    }
+    public void push(int element){
+        if (isFull()) {
+            throw new StackOverflowError("Stack is full");
+        }
+        arr[index] = element;
+        index++;
+    }
+
+
+
+    public int size() {
+        return index;
+    }
+
+    public boolean isFull() {
+        if (index == size) {
+            return true;
+        }
+        return false;
+    }
+
+}
